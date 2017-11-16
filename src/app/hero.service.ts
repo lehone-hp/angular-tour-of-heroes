e9bae7267e897653ecd6913c28c212bbb29a18a7
Lenya Hope <lehone4hope@njorku.com>
2017-11-16 13:05:42 +0100

    MessageComponent: Added MessageComponent and MessageService for data acess

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 import  { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
@@ -9,9 +10,10 @@ import { of } from 'rxjs/observable/of';
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
